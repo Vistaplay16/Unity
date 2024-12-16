835ba0b5b063819cc839f844488bd619a000b656
Victor <124076416+Vistaplay16@users.noreply.github.com>
2024-12-16 01:10:43 +0100

    Primer comit

---
+++ Assets/meshTriangles.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+public class meshTriangles : MonoBehaviour
+{
+	// Use this for initialization
+	void Start() {
+        gameObject.AddComponent<MeshFilter>();
+        gameObject.AddComponent<MeshRenderer>();
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        mesh.Clear();
+        mesh.vertices = new Vector3[] {new Vector3(0, 0, 0), new Vector3(0, 1, 0), new Vector3(1, 1, 0)};
+        mesh.uv = new Vector2[] {new Vector2(0, 0), new Vector2(0, 1), new Vector2(1, 1)};
+        mesh.triangles = new int[] {0, 1, 2};
+    }
+	 
+    void Update () {
+	
+	}
+}
